0ecf54535e630e2141a310ac4da5e39ddb5195de
TinguelySacha <147831878+TinguelySacha@users.noreply.github.com>
2023-10-18 08:40:54 +0200

    Add files via upload

--- recup_scores.php
+++ recup_scores.php
@@ -10,14 +10,11 @@ if ($conn->connect_error) {
     die("La connexion à la base de données a échoué : " . $conn->connect_error);
 }
 
-// Récupérez les données du score de l'utilisateur et son ID de la requête POST
 $userId = $_POST["userId"];
 
-// Initialisation des variables
 $otherUserId = null;
 $otherUserScore = null;
 
-// Recherchez la salle de l'utilisateur
 $sql = "SELECT room_id FROM room_users WHERE id = $userId";
 $result = $conn->query($sql);
 
@@ -25,7 +22,6 @@ if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
     $roomId = $row["room_id"];
 
-    // Maintenant, récupérez l'ID de l'autre utilisateur dans la même salle
     $sql = "SELECT id FROM room_users WHERE room_id = $roomId AND id != $userId";
     $result = $conn->query($sql);
 
@@ -35,7 +31,6 @@ if ($result->num_rows > 0) {
     }
 }
 
-// Récupérez le score de l'autre utilisateur
 if ($otherUserId !== null) {
     $sql = "SELECT score FROM scores WHERE user_id = $otherUserId";
     $result = $conn->query($sql);
@@ -46,13 +41,11 @@ if ($otherUserId !== null) {
     }
 }
 
-// Créez un tableau avec les données à renvoyer
 $response = [
     "otherUserId" => $otherUserId,
     "otherUserScore" => $otherUserScore
 ];
 
-// Définissez l'en-tête de réponse pour indiquer que c'est du JSON
 header('Content-Type: application/json');
 echo json_encode($response);
 
